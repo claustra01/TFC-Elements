bc05a7bcf11b8d9dba86ae185c156d754f01db2b
sny430 <shinya430.g@gmail.com>
2023-01-04 22:15:43 +0900

    :tada: initial commit

---
+++ src/main/java/claustra01/tfcelements/TFCElements.java
@@ -0,0 +1,10 @@
+package claustra01.tfcelements;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("tfcelem")
+public class TFCElements {
+
+    public static final String namespace = "tfc";
+
+}
