73f61cb06f464b345af484d4f8006dc007079bcb
sny430 <shinya430.g@gmail.com>
2023-01-04 23:04:27 +0900

    :sparkles: add creative tab

---
+++ src/main/java/claustra01/tfcelem/TFCEItemGroup.java
@@ -0,0 +1,30 @@
+package claustra01.tfcelem;
+
+import net.dries007.tfc.common.blocks.rock.Ore;
+import net.dries007.tfc.common.capabilities.food.FoodCapability;
+import net.dries007.tfc.common.items.TFCItems;
+import net.dries007.tfc.util.Metal;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.common.util.Lazy;
+
+import javax.annotation.Nonnull;
+import java.util.function.Supplier;
+
+public class TFCEItemGroup extends CreativeModeTab {
+
+    public static final CreativeModeTab METAL = new TFCEItemGroup("metals", () -> new ItemStack((TFCItems.METAL_ITEMS.get(Metal.Default.WROUGHT_IRON)).get(Metal.ItemType.INGOT).get()));
+    public static final CreativeModeTab ORES = new TFCEItemGroup("ores", () -> new ItemStack((TFCItems.GRADED_ORES.get(Ore.NATIVE_COPPER).get(Ore.Grade.NORMAL)).get()));
+
+    private final Lazy<ItemStack> iconStack;
+
+    private TFCEItemGroup(String label, Supplier<ItemStack> iconSupplier) {
+        super(TFCElements.namespace + "." + label);
+        this.iconStack = Lazy.of(() -> FoodCapability.setStackNonDecaying(iconSupplier.get()));
+    }
+
+    @Nonnull
+    public ItemStack makeIcon() {
+        return this.iconStack.get();
+    }
+}
