671c4b3d8d6327d72ab58c55924b5bec408d1d4f
sny430 <shinya430.g@gmail.com>
2023-01-05 20:59:23 +0900

    :truck: rename print->gen

---
+++ utils/generator.py
@@ -0,0 +1,5 @@
+import gen_models
+import gen_states
+
+gen_models.ore_model_gen()
+gen_states.ore_blockstate_gen()
--- utils/print.py
+++
@@ -1,5 +0,0 @@
-import print_models
-import print_states
-
-print_states.ore_blockstate_gen()
-print_models.ore_model_gen()
