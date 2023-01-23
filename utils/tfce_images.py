6b6535545d86170f92fee852de15a7d20879bd91
sny430 <shinya430.g@gmail.com>
2023-01-23 14:14:22 +0900

    :recycle: separete transpare func

--- utils/tfce_images.py
+++ utils/tfce_images.py
@@ -1,7 +1,14 @@
 import numpy as np
 import cv2
 
-# Change HSV
+def transpare(bgr_img, bgr_mask, div_range):
+    mask = np.all((bgr_img[:,:,:] - bgr_mask) <= div_range, axis=-1)
+    bgra_img = cv2.cvtColor(bgr_img, cv2.COLOR_BGR2BGRA)
+    bgra_img[mask, 3] = 0
+    return bgra_img
+
+
+# change HSV
 def change_hsv(temp_path, file_path, h, s, v):
     
     temp_img = cv2.imread(temp_path)
@@ -10,16 +17,12 @@ def change_hsv(temp_path, file_path, h, s, v):
     hsv_img[:,:,(0)] = hsv_img[:,:,(0)] + h
     hsv_img[:,:,(1)] = hsv_img[:,:,(1)] + s
     hsv_img[:,:,(2)] = hsv_img[:,:,(2)] + v
-    
-    hsv_white = [0, 0, 255]
-    hsv_mask = [hsv_white[0]+h, hsv_white[1]+s, hsv_white[2]+v]
+    bgr_img = cv2.cvtColor(hsv_img, cv2.COLOR_HSV2BGR)
+   
+    hsv_black = [0, 0, 0]
+    hsv_mask = [hsv_black[0]+h, hsv_black[1]+s, hsv_black[2]+v]
     bgr_mask = cv2.cvtColor(np.array([[hsv_mask]], dtype=np.uint8), cv2.COLOR_HSV2BGR)[0][0]
     
-    bgr_img = cv2.cvtColor(hsv_img, cv2.COLOR_HSV2BGR)
-    mask = np.all(bgr_img[:,:,:] == bgr_mask, axis=-1)
-    bgra_img = cv2.cvtColor(bgr_img, cv2.COLOR_BGR2BGRA)
-    bgra_img[mask, 3] = 0
-    
+    bgra_img = transpare(bgr_img, bgr_mask, 0)
     cv2.imwrite(file_path, bgra_img)
-
 
