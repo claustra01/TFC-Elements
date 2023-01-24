4e21a03c2e89f4e8a23a21bd91fdf1817cc2cc11
sny430 <shinya430.g@gmail.com>
2023-01-24 14:15:24 +0900

    :technologist: add small ore pieces tag generator

--- utils/ore_all.py
+++ utils/ore_all.py
@@ -11,3 +11,11 @@ def register_tags():
         for grade in tfce_types.grades:
             values.append("tfc:ore/" + grade + "_" + ore[0])
     tfce_utils.create_simple_tag(dir_path, file_path, values)
+    
+    # small ores
+    values = []
+    dir_path = "../src/main/resources/data/tfc/tags/items"
+    for ore in tfce_types.ores:
+        values.append("tfc:ore/small_" + ore[0])
+    tfce_utils.create_simple_tag(dir_path, dir_path + "/small_ore_pieces.json", values)
+    tfce_utils.create_simple_tag(dir_path, dir_path + "/nuggets.json", values)
